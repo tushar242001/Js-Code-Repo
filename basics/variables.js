1c281a1dd01d70bf33740abe67ccddd6ae8915fe
tushar242001 <tusharadling24@gmail.com>
2024-01-24 18:49:31 +0530

    Initial Update

--- basics/variables.js
+++ basics/variables.js
@@ -3,4 +3,9 @@ let accountEmail ="tusharadling24@gmail.com"
 var accountPassword ="12345"
 accountCity="Indapur" 
 
+/*
+Prefer not to use var 
+Because of issue in block scope and functional scope 
+*/
+
 console.log(accountCity)
